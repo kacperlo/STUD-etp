beaf2621a3d10eadc184f87cacf1cac04775ecf5
Kacper Lobodecki <kaspereo@gmail.com>
2021-11-29 15:24:28 +0100

    Clean code and fix error in task2 formula

--- src/Tasks/Task2.svelte
+++ src/Tasks/Task2.svelte
@@ -1,25 +1,16 @@
 <script>
-    import Chart from "../Chart.svelte";
-    import SvelteTable from "svelte-table";
-
     let dlFali = 910;
     let temp_dry = 5;
     let temp_wet = 5;
     let pa = 1013.25;
     let d = 1000;
 
-    let nnn = dlFali/1000
-
-    let Ng0 = 287.6155 + 4.8866 / (dlFali / 1000) ** 2 + 0.0680 / (dlFali / 1000) ** 4
-    let Ew = 6.1087 * Math.E ** (17.269 * temp_wet / (237.30 + temp_dry))
-    let w_pressure = Ew - 0.000662 * pa * (temp_dry - temp_wet)
-    let Ng = Ng0 * 0.2695780903034789 * pa / (temp_dry + 273.15) - (11.27 * w_pressure / (temp_dry + 273.15))
-
     let T_ns=288.15
     let P_ns=1013.25
     let e_ns=10.87
-    let Ng_s=Ng0*0.269578*P_ns/T_ns - (11.27*e_ns/T_ns)
-    $: poprawka = (287.6155 + 4.8866 / (dlFali / 1000) ** 2 + 0.0680 / (dlFali / 1000) ** 4)*0.269578*P_ns/T_ns - (11.27*e_ns/T_ns) - (287.6155 + 4.8866 / (dlFali / 1000) ** 2 + 0.0680 / (dlFali / 1000) ** 4) * 0.2695780903034789 * pa / (temp_dry + 273.15) - (11.27 * w_pressure / (temp_dry + 273.15))
+
+    $: poprawka = ((287.6155 + (4.8866 / (dlFali / 1000) ** 2 + 0.0680 / (dlFali / 1000) ** 4))*0.269578*P_ns/T_ns - (11.27*e_ns/T_ns)) - ((287.6155 + 4.8866 / (dlFali / 1000) ** 2 + 0.0680 / (dlFali / 1000) ** 4) * 0.269578 * pa / (temp_dry + 273.15) - (11.27 * ((6.1087 * (Math.E ** ((17.269 * temp_wet) / (237.30 + temp_wet)))) - (0.000662 * pa * (temp_dry - temp_wet))) / (temp_dry + 273.15)))
+
 </script>
 
 <div style="width: 50%; float: left">
